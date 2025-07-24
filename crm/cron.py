09d166a57ddea0e95168773cb989723749c2477c
MathiasDigit <bellmathias33@gmail.com>
2025-07-24 14:41:39 +0100

    modification the file cron.py, settings.py and crm_heartbeat_log.txt

--- crm/cron.py
+++ crm/cron.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from gql.transport.requests import RequestsHTTPTransport
+from gql import gql, Client
 
 def log_crm_heartbeat():
     now = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
     message = f"{now} CRM is alive\n"
 
-    with open("/crom/crm_heartbeat_log.txt", "a") as f:
+    with open("/tmp/crm_heartbeat_log.txt", "a") as f:
         f.write(message)
